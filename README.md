0c7fd55c13f21086bc586f24f4fc46b68b8540d3
mdgmaster <sergiomadrigal@gmail.com>
2020-10-29 21:57:38 +0100

    Version 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # eos
 EOS I Project
+
+Log Version
+
+## Version 0.1a
+-----------------------
+
--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
-
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
